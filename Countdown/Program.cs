e018fcb46affd5fc3fbbb4f0fd562441bdf65648
jb-network <85036487+jb-network@users.noreply.github.com>
2022-05-25 22:26:04 -0500

    Add project files.

---
+++ Countdown/Program.cs
@@ -0,0 +1,26 @@
+﻿// This is challenge work for the "C# Players Guide"
+// Level 13 the countdown Challenge
+// This is a simple task to create a count down from 10 to 1 without using loops.
+// The goal is to use recursion
+
+int countDownNumber = 10;
+
+CountDownMethod(countDownNumber);
+
+void CountDownMethod(int countDownNumber)
+{
+    Console.WriteLine(countDownNumber);
+
+   if (countDownNumber == 1) 
+    {
+        return;
+    }
+   else
+    {
+        countDownNumber--;
+        CountDownMethod(countDownNumber);
+    }
+}
+
+Console.WriteLine("Press any key to end recursive countdown program");
+Console.ReadKey();
